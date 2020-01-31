0561f0d86f4d1b07cab4ff90cc1bc94532f223d0
kimHJ <khj64076407@gmail.com>
2020-01-31 16:58:27 +0900

    datepicker IOS

--- button/App.js
+++ button/App.js
@@ -1,19 +1,37 @@
-import React from "react";
-import { StyleSheet, Text, View, Switch } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, DatePickerIOS, Text, View, Switch } from "react-native";
+// import DateTimePicker from "@react-native-community/datetimepicker";
 
-export default class App extends React.Component {
+export default class App extends Component {
   state = { switchValue: false };
   toggleSwitch = value => {
     this.setState({ switchValue: value });
   };
+  constructor(props) {
+    super(props);
+    this.state = { chosenDate: new Date() };
+
+    this.setDate = this.setDate.bind(this);
+  }
+
+  setDate(newDate) {
+    this.setState({ chosenDate: newDate });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.switchValue ? "Dark Mode" : "White Mode"}</Text>
-        <Switch
-          style={{ marginTop: 30 }}
-          onValueChange={this.toggleSwitch}
-          value={this.state.switchValue}
+        <View style={styles.switch}>
+          <Text>{this.state.switchValue ? "Dark Mode" : "White Mode"}</Text>
+          <Switch
+            style={{ marginTop: 30 }}
+            onValueChange={this.toggleSwitch}
+            value={this.state.switchValue}
+          />
+        </View>
+        <DatePickerIOS
+          date={this.state.chosenDate}
+          onDateChange={this.setDate}
         />
       </View>
     );
@@ -22,7 +40,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
+    justifyContent: "center"
+  },
+  switch: { justifyContent: "center", alignItems: "center" }
 });
